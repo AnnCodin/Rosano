dd3766d1dd654f0f3ef8de5fc5323adbcb5879d8
AnnCodin <qantoinnette23@gmail.com>
2026-05-18 22:08:20 +0000

    Add topbar

---
+++ src/Components/Topbar/Topbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Topbar.css";
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <div className="container topbar-inner">
+        <div className="topbar-left">
+          <span>CONTACT US</span>
+          <span>OUR LOCATIONS</span>
+          <span>FAQs</span>
+          <span>MEDIA CENTER</span>
+        </div>
+        <div className="topbar-right">
+          <span>
+            24/7 Banking Service: +1(234)456-0 <i class="ri-search-line"></i>
+          </span>
+          <i class="ri-twitter-fill"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
---
+++ src/Layout/RootLayout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+import Topbar from "../Components/Topbar/Topbar";
+import Footer from "../Components/Footer/Footer";
+
+const RootLayout = () => {
+  return (
+    <div>
+      <Topbar />
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default RootLayout;
